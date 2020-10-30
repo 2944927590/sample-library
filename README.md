5569da832cdcd0873f1aea560890e1f50ee8acb0
xiezhiqiang02 <xiezhiqiang02@meituan.com>
2020-10-30 16:13:31 +0800

    add config

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sample-library
 sample library
+
+项目 build 之后的静态资源，可以被 gitpage 托管，用于在线演示
+
---
+++ gen.js
@@ -0,0 +1,7 @@
+const config = require('./config.json');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+console.log(process.env.project)
+
