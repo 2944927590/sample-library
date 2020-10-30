17deec7115e0a2a857486d5b084b77b787e2aba3
xiezhiqiang02 <xiezhiqiang02@meituan.com>
2020-10-30 19:33:17 +0800

    添加自动拷贝脚本

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # sample-library
-sample library
+样例库
 
-项目 build 之后的静态资源，可以被 gitpage 托管，用于在线演示
+`npm install` 安装依赖
+
+项目 `build` 之后的静态资源，可以被 `gitpage` 托管，用于在线演示
+
+在 `config.json` 中配合项目的信息
+
+project： 项目名称
+
+from：项目 `build` 之后的目录（拷贝源）
+
+to：拷贝至当前项目的位置（目标）
+
+在 `.env` 文件中配置 `project` 名称，通过 `npm run gen` 来执行拷贝
+
+也可通过命令行 `project=XXX npm run gen` 来执行拷贝
 
--- gen.js
+++ gen.js
@@ -1,7 +1,36 @@
-const config = require('./config.json');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
+const config = require('./config.json');
+const fsEx = require('fs-extra');
+const path = require('path');
 
-console.log(process.env.project)
+console.log(process.env.project);
+
+async function copyFolder(sourcePath, targetPath) {
+  try {
+    await fsEx.copy(sourcePath, targetPath);
+    console.log(`复制成功：${sourcePath} => ${targetPath}`);
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+function start() {
+  const projectName = process.env.project;
+  const targetConfig = config.find(item => item.project === projectName);
+  const sourcePath = path.join(__dirname, targetConfig.from);
+  const targetPath = path.join(__dirname, targetConfig.to);
+
+  if (!projectName) {
+    console.log('projectName 为空，示例： project=online-editor npm run gen');
+    return;
+  }
+  console.log(targetConfig);
+  if (!targetConfig) {
+    console.log('配置文件发生错误');
+    return;
+  }
+  copyFolder(sourcePath, targetPath);
+}
+
+start();
 
